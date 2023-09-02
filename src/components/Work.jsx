4487fee1b99bddf8900c5d3186df5b9e49b4439d
Jessel2023 <velasco.jesseljoy1@gmail.com>
2023-09-02 23:05:38 +0800

    please work

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -29,7 +29,7 @@ const data =[
 const Work = () => {
   return (
     <div id='work' className='max-w-[1040px] m-auto md:pl-20 p-4 py-16'>
-            <h1 className='text-4xl font-bold text-center text-[#001b5e] p-4'>Work/Experience</h1>
+            <h1 className='text-4xl font-bold text-center text-[#001b5e] p-4'>Works and Experience</h1>
         {data.map((item, idx) => (
             <WorkItem 
             key={idx} 
